141db46ff61eafeada08e9160fd622e0ffba526e
Roshni Rathore <roshnirathore099@gmail.com>
2026-05-05 21:37:58 +0530

    Implement digit counting for divisibility check
    Added a function to count the digits of a number that divide it.

--- 1. Basics/ALL-DIVISORS.py
+++ 1. Basics/ALL-DIVISORS.py
@@ -1,1 +1,26 @@
+# Count The Digit that Divide a Number
+'''Example 1:
 
+Input: num = 7
+Output: 1
+Explanation: 7 divides itself, hence the answer is 1.
+Example 2:
+
+Input: num = 121
+Output: 2
+Explanation: 121 is divisible by 1, but not 2. Since 1 occurs twice as a digit, we return 2.
+Example 3:
+
+Input: num = 1248
+Output: 4
+Explanation: 1248 is divisible by all of its digits, hence the answer is 4.'''
+
+      def countDigits(self, num: int) -> int:
+        count=0
+        n=num
+        while num>0:
+            digit=num%10
+            if n%digit==0:
+                count+=1
+            num=num//10
+        return count
