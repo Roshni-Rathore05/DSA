86643af45948e0d2e26e246afeac3e4e866ddfa0
Roshni Rathore <roshnirathore099@gmail.com>
2026-05-08 10:02:47 +0530

    Add function to count factors of a number
    Implement factor counting function for a number.

---
+++ 1. Basics/Factor_of_number.py
@@ -0,0 +1,21 @@
+Examples:
+
+Input: n = 5
+Output: 2
+Explanation: 5 has 2 factors 1 and 5
+Input: n = 25
+Output: 3
+Explanation: 25 has 3 factors 1, 5, 25 
+
+from math import sqrt
+class Solution:
+    def countFactors (self, n):
+        count = 0
+        for i in range(1, int(sqrt(**0.5)) + 1):
+            if n % i == 0:
+                if i == n // i:
+                    count += 1   # perfect square case
+                else:
+                    count += 2   # pair of factors
+
+        return count
